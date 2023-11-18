f2d00d1894b6aa58d09cc035e74760ad3e18b80c
meidix <mahdihossieni@gmail.com>
2023-11-18 18:14:33 +0330

    refactored server/main to use the new modules

--- server/main.go
+++ server/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"meidix/chatserv/chat"
+	"meidix/chatserv/connection"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -23,10 +24,9 @@ func main() {
 
 	users := map[string]net.TCPConn{}
 
+	gm := make(chan chat.Messege)
 
-	gm := make(chan string)
-
-	go messegeDispatcher(users, gm)
+	go chat.MessegeDispatcher(users, gm)
 
 	for {
 		conn, err := l.AcceptTCP()
@@ -34,47 +34,17 @@ func main() {
 			fmt.Println("There was a problem in establishing the connection")
 			os.Exit(1)
 		}
-		rbuff := make([]byte, 1024)
-		wbuff := []byte("Hello There, What is your name: ")
-		conn.Write(wbuff)
-		_, err = conn.Read(rbuff)
-		if err != nil {
-			fmt.Println("There was a problem reading response")
+
+		err = connection.SendTxtMsg(conn, "Hello There, What is you name: "); if err != nil {
+			fmt.Println("There was a problem sending a text messege", err)
+		}
+
+		user, err := connection.RecvTxtMsg(conn); if err != nil {
+			fmt.Println("There was a problem receiving the messege")
 			os.Exit(1)
 		}
-		setupChat(rbuff, conn, users, gm)
-		fmt.Println("user connected", strings.Trim(string(rbuff), "\x00"), "connected")
+		chat.SetupChat(user, conn, users, gm)
+		fmt.Println("user connected", user, "connected")
 
-	}
-}
-
-func setupChat(buff []byte, conn *net.TCPConn, u map[string]net.TCPConn, ch chan string) {
-	key := strings.Trim(string(buff), "\x00")
-	u[key] = *conn
-	go messegeListener(key, *conn, ch)
-	conn.Write([]byte("Welcome to This Chat"))
-}
-
-
-func messegeListener(u string, conn net.TCPConn, ch chan string) {
-	for {
-		rbuff := make([]byte, 1024)
-		_, err := conn.Read(rbuff); if err != nil {
-			fmt.Println("There was a problem receiving messege")
-			os.Exit(1)
-		}
-		ch <- strings.Trim(string(rbuff), "\x00")
-	}
-}
-
-
-func messegeDispatcher(u map[string]net.TCPConn, ch chan string) {
-	for m := range ch {
-		for key, conn := range u {
-			_, err := conn.Write([]byte(m)); if err != nil {
-				fmt.Println("there was a problem sending messeges")
-				delete(u, key)
-			}
-		}
 	}
 }
